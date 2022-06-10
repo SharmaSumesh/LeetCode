e219e2d00c1382658532c2a86418bdba0afd8bb7
SharmaSumesh <97055356+SharmaSumesh@users.noreply.github.com>
2022-06-10 23:53:16 +0530

    Create 344. Reverse String.java

---
+++ 344. Reverse String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        for(int i =0;i<s.length/2;i++)
+        {
+            char s1 = s[i];
+            s[i] = s[s.length-i-1];
+            s[s.length-i-1] = s1;
+        }
+        
+    }
+}
